beb7931c1e0545230fe27dd567eb9bd9a773694c
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-10-21 15:39:41 -0500

    commit tree 5baf2792ae84b86207f46c4867b8a7b7a22267b0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,26 @@
-class Student 
+class Student < User
+  def initialize 
+    @knowledge = []
+  end
 
+  def learn(string) 
+    @knowledge << string 
+  end
+
+  def knowledge 
+    @knowledge
+  end
 end
+
+# class Student < User
+#   attr_reader :knowledge
+
+#   def initialize
+#     @knowledge = []
+#   end
+
+#   def learn(knowledge)
+#     @knowledge << knowledge
+#   end
+
+# end
